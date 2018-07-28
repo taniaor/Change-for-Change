25562b9f064055fb6c1ad4e5558cf0da335b56cf
Tania Ortiz-Rosales <tania_or58@yahoo.com>
2018-07-28 15:01:59 -0700

    Install Firebase

---
+++ Change for Change/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Change for Change
+//
+//  Created by Tania Ortiz-Rosales on 7/28/18.
+//  Copyright © 2018 Tania Ortiz-Rosales. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseUI
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+}
